5d17365b925ddcf7a00f3e71704f85d307b366cc
Rub3nGM <rubengmoura@gmail.com>
2013-08-16 01:51:52 +0100

    initial

---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+"http://www.w3.org/TR/html4/loose.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+		<title >Materiare</title>
+
+		<link rel="stylesheet" type="text/css" href="/Materiare/css/base.css"  />
+		<link rel="stylesheet" type="text/css" href="/Materiare/css/contentflow.css"/>
+		<script type="text/javascript" src="/Materiare/js/contentflow.js" ></script>
+		<!-- <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script> -->
+
+	</head>
+		<?php include "header.html"; ?>
+	<body>
+		<div id="main">
+
+			<div class="ContentFlow" style="margin-top: 100px;" id="myFantasicFlow" >
+				<div class="loadIndicator">
+					<div class="indicator"></div>
+				</div>
+				<div class="flow">
+					<img class="item" src="images/coverflow/portas.jpg" href="#" target="#" title="Portas" />
+					<img class="item" src="images/coverflow/projectos.jpg" title="Projectos"/>
+					<img class="item" src="images/coverflow/moveis_wc.jpg" title="Móveis WC"/>
+					<img class="item" src="images/coverflow/cozinhas.jpg" title="Cozinhas" href="cozinhas.html" target="_top"/>
+					<img class="item" src="images/coverflow/moveis_por_medida.jpg" title="Móveis por Medida" href="#" />
+					<img class="item" src="images/coverflow/decoraçao_madeira.jpg" title="Decoração em Madeira"/>
+					<img class="item" src="images/coverflow/locais_publicos_comercio.jpg" title="Locais Públicos e Comércio"/>
+					<img class="item" src="images/coverflow/escadas.jpg" title="Escadas"/>
+				</div>
+				<div class="globalCaption">
+
+				</div>
+				<div class="scrollbar" >
+					<div class="preButton"></div>
+					<div class="nextButton"></div>
+					<div class="slider">
+						<div class="position"></div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<?php include "footer.html"; ?>
+
+	</body>
+</html>
---
+++ sites/portfolio/cozinhas.php
@@ -0,0 +1,49 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+"http://www.w3.org/TR/html4/loose.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+		<title >Materiare</title>
+
+		<link rel="stylesheet" type="text/css" href="/Materiare/css/base.css"  />
+		<link rel="stylesheet" type="text/css" href="/Materiare/css/contentflow.css"/>
+		<script type="text/javascript" src="/Materiare/js/contentflow.js" ></script>
+		<!-- <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script> -->
+
+	</head>
+	<body>
+		<?php include($_SERVER['DOCUMENT_ROOT'].'/Materiare/header.html'); ?>
+
+		<div id="main">
+
+			<div class="ContentFlow" style="margin-top: 100px;" id="myFantasicFlow" >
+				<div class="loadIndicator">
+					<div class="indicator"></div>
+				</div>
+				<div class="flow">
+					<img class="item" src="images/portfolio/cozinhas/castanho.jpg" target="#" title="Castanho" href="/Materiare/sites/portfolio/cozinhas/castanho.php" />
+					<img class="item" src="images/portfolio/cozinhas/branco.jpg" title="Branco" href="/Materiare/sites/portfolio/cozinhas/branco.php"/>
+					<img class="item" src="images/portfolio/cozinhas/preto.jpg" title=" Preto" href="/Materiare/sites/portfolio/cozinhas/preto.php"/>
+					<img class="item" src="images/portfolio/cozinhas/inox.jpg" title="Inox" target="_top" href="/Materiare/sites/portfolio/cozinhas/inox.php" />
+					<img class="item" src="images/portfolio/cozinhas/verde.jpg" title="Verde" href="/Materiare/sites/portfolio/cozinhas/verde.php"  />
+					<img class="item" src="images/portfolio/cozinhas/vermelho.jpg" title="Vermelho" href="/Materiare/sites/portfolio/cozinhas/vermelho.php" />
+					<img class="item" src="images/portfolio/cozinhas/outros.jpg" title="Outros" href="/Materiare/sites/portfolio/cozinhas/outros.php" />
+				</div>
+				<div class="globalCaption">
+
+				</div>
+				<div class="scrollbar" >
+					<div class="preButton"></div>
+					<div class="nextButton"></div>
+					<div class="slider">
+						<div class="position"></div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+	<?php include($_SERVER['DOCUMENT_ROOT'].'/Materiare/footer.html'); ?>
+	</body>
+</html>
